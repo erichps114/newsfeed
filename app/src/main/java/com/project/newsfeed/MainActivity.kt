9e314ce0d3661daa950df44938ddfd22889b874e
ErichPS <erichpratamasaputra@gmail.com>
2019-03-27 01:01:45 +0700

    First working hit API

--- app/src/main/java/com/project/newsfeed/MainActivity.kt
+++ app/src/main/java/com/project/newsfeed/MainActivity.kt
@@ -3,6 +3,7 @@ package com.project.newsfeed
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import com.project.newsfeed.model.ResponseModel
 import com.project.newsfeed.rest.ApiInterface
@@ -23,10 +24,19 @@ class MainActivity : AppCompatActivity() {
         val callApi = ApiInterface.instance.searchArticles("indonesia",0)
         callApi.enqueue(object : Callback<ResponseModel>{
             override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
-
+                Log.e("ERICH", "ERROR")
             }
 
             override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                if (response.code() != 200) {Log.e("ERICH","Failed"); return}
+                response.body()?.response?.docs?.forEach {
+                    Log.i("ERICH","""
+                    Headline : ${it.headline}
+                    Snipper : ${it.snippet}
+                    Web_Url : ${it.web_url}
+                    Pub_date : ${it.pub_date}
+                """.trimIndent())
+                }
 
             }
         })
--- app/src/main/java/com/project/newsfeed/model/ResponseModel.kt
+++ app/src/main/java/com/project/newsfeed/model/ResponseModel.kt
@@ -1,7 +1,26 @@
 package com.project.newsfeed.model
 
 import com.google.gson.annotations.Expose
+import java.io.Serializable
 
 class ResponseModel {
-
+    @Expose
+    var response : Container = Container()
 }
+
+class Container {
+    @Expose
+    var docs : List<NewsModel> = mutableListOf()
+}
+
+class HeadLineModel{
+    @Expose
+    var main : String = ""
+}
+
+data class NewsModel (
+    var web_url : String = "",
+    var snippet : String ="",
+    var pub_date : String ="",
+    var headline : HeadLineModel = HeadLineModel()
+) : Serializable
